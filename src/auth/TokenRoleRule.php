c09e1b8bd7e9d69cc847cc08676045ba32934da1
a.klapper <a.klapper@herzogkommunikation.de>
2024-04-17 14:03:53 +0200

    added support for nested claims in token

--- src/auth/TokenRoleRule.php
+++ src/auth/TokenRoleRule.php
@@ -41,11 +41,51 @@ class TokenRoleRule extends Rule
             // If access token is null, user is not authenticated via keycloak
             if ($accessToken instanceof OAuthToken) {
                 // Extract groups from access token and check if rbac item is in list
-                $roles = $accessToken->getParam($this->rbacRolesClaimName) ?? [];
+                $roles = (array)$this->getClaim($this->rbacRolesClaimName, $accessToken);
                 return in_array($item->name, $roles);
             }
         }
         return false;
+    }
+
+    /**
+     * Get claims from the token.
+     * For nested claims, IE: "groups": ["a" : ["B", "C"]] use dot notation for $name: groups.a
+     * @param string $name
+     * @param $default
+     * @return mixed
+     */
+    protected function getClaim(string $name, OAuthToken $accessToken)
+    {
+        // split name into separate parts
+        $parts = explode('.', $name);
+
+        // check if there is at least one item
+        $baseName = $parts[0] ?? null;
+        if ($baseName === null) {
+            return null;
+        }
+
+        // remove first part because it is saved in $baseName
+        unset($parts[0]);
+
+        // set this as base value
+        $baseValue = $accessToken->getParam($baseName);
+
+        // iterate over the rest of the parts
+        foreach ($parts as $part) {
+            // check if key exists. If not return default.
+            if (!isset($baseValue[$part])) {
+                return null;
+            }
+            // check if value is array to continue. If not return value
+            if (!is_array($baseValue[$part])) {
+                return $baseValue;
+            }
+            $baseValue = $baseValue[$part];
+        }
+        // return the value
+        return $baseValue;
     }
 }
 
